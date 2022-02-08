d3d82c856f0a4d3a88a2d9233f8b770e5cd3e932
Santiago Restrepo <santirstrp@gmail.com>
2022-02-08 17:05:39 -0500

    fix/featureLogin

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link , useHistory, Redirect } from 'react-router-dom';
 import { useForm } from "react-hook-form";
 import logo from '../../assets/logo.svg'
