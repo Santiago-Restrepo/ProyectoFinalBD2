acee41ccd01db53d7ecf9b5424c01bbebf0aa2a4
Juan Galvis <43938697+JuanesGalvis@users.noreply.github.com>
2022-02-08 15:22:17 -0500

    Páginas para recovery

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Context } from './Context';
+
+/** Páginas */
 import { Register } from './pages/Register'
 import { Login } from './pages/Login'
+import { Recovery } from './pages/Login/Recovery'
+import { ChangePassword } from './pages/Login/ChangePassword'
 import { Home } from './pages/Home'
 import { Profile } from './pages/Profile'
 import { EvaluationPlan } from './pages/EvaluationPlan';
 import { NotFound } from './pages/NotFound'
-import { Context } from './Context';
-
-/** Componentes */
 
 /** Estilos globales */
 import './global.sass';
@@ -30,6 +32,8 @@ export const App = () => {
                         <Route exact path="/" component={Register} />
                         <Route exact path="/home" component={Home} />
                         <Route exact path="/login" component={Login} />
+                        <Route exact path="/recovery" component={Recovery} />
+                        <Route exact path="/change_password" component={ChangePassword} />
                         <Route exact path="/evaluation-plan" component={EvaluationPlan} />
                         <Route exact path="/profile" component={Profile} />
                         <Route path="*" component={NotFound} />
--- src/pages/Login/Recovery.jsx
+++ src/pages/Login/Recovery.jsx
@@ -4,7 +4,7 @@ import { useForm } from "react-hook-form";
 import logo from '../../assets/logo.svg'
 import { Context } from '../../Context';
 
-export const Login = () => {
+export const Recovery = () => {
 
     const history = useHistory();
     const {setUserAutentication} = useContext(Context);
@@ -13,28 +13,20 @@ export const Login = () => {
     const queryDatabase = async (data) => {
         /*Consulta a la base de datos*/
         try {
-            
-            const user = {
-                data: '',
-                logged: false
-            };
     
-            const response = await fetch('https://paseraspandoapi.vercel.app/login',{
+            const response = await fetch('https://paseraspandoapi.vercel.app/recovery',{
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 method: 'POST',
                 body: JSON.stringify({
                     email: data.email,
-                    password: data.password
                 })
             });
     
             const jsonResponse = await response.json();
-            setUserAutentication(jsonResponse);
-            
-            history.push('/home')
-
+            alert(jsonResponse.message);
+            history.push('/login')
             
         } catch (error) {
             alert('Usuario o contraseña incorrectos')
@@ -51,22 +43,17 @@ export const Login = () => {
                 <h1>Nombre de la app</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum iste, iusto temporibus vel sapiente architecto dolor expedita est, at ducimus aliquid earum illo minus dolorem.</p>
             </section>
-            {
-                !userLogged ?
-                <>
-                    <form className="registerForm" onSubmit={handleSubmit(queryDatabase)}>
-                        <h1>Ingresa ahora</h1>
-                        <p>¿No tienes una cuenta? <Link to="/">Regístrate</Link></p>
-                        <label htmlFor="email">Correo electrónico</label>
-                        <input input="email" type="mail" {...register("email")} />
-                        <label htmlFor="password">Contraseña</label>
-                        <input {...register("password")} id="password" type="password"/>
-                        <input className='submitButton' type='submit' value="Iniciar sesión"/>
-                        <Link to="/recovery">¿No recuerdas tu contraseña?</Link>
-                    </form>
-                </>
-                : <Redirect to='/home'/>
-            }
+            <>
+                <form className="registerForm" onSubmit={handleSubmit(queryDatabase)}>
+                    <h1>Recuperar contraseña</h1>
+                    <p>¿No tienes una cuenta? <Link to="/">Regístrate</Link></p>
+                    <label htmlFor="email">Correo electrónico</label>
+                    <input input="email" type="mail" {...register("email")} />
+                    <input className='submitButton' type='submit' value="Recuperar"/>
+                    <Link to="/login">¿Ya recordaste tu contraseña?</Link>
+                    <Link to="/change_password/55">Prueba</Link>
+                </form>
+            </>
         </main>
     );
 }
